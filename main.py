add1a721629ca37c995cb079cbc1ae51e18d2019
Shaurya <shauryagarg76@gmail.com>
2026-04-05 04:04:48 +0530

    tested elevations mapped to the cells added a displayCell function to check if it is rendering properly

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     maps = spaces.Map((256, 256), False, 400, 2)
     wind = winds.OnlySouthwardWinds(256, 256)
     maps.create_biomes(wind)
-
+    maps.displayCell(8)
 
 if __name__ == "__main__":
     main()
